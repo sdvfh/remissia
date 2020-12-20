0d8e983df98e2fca7f379cf9f404ca0c8f79ede7
Sérgio de Vasconcelos Filho <sergio.vasconcelos@mv.com.br>
2020-12-19 22:06:04 -0300

    Uso de pandas para pré-processamento e anotação dos estudos

---
+++ codigos/criar_tabela_principal_pandas.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import glob
+import numpy as np
+
+path = r"C:\arquivos_spark\SEER_1975_2016_CUSTOM_TEXTDATA\incidence"
+paths = glob.glob(path + "\**\*.txt", recursive=True)
+
+dicionario = pd.read_csv(r'D:\repositorios\remissia\dicionarios\carga_inicial.csv', sep=';')
+
+dfs = []
+for path in paths:
+    df = pd.read_csv(path, header=None)
+    for variavel in dicionario.iterrows():
+        nome = variavel[1]['Name']
+        pos = variavel[1]['Position']
+        tam = variavel[1]['Length']
+        tipo = variavel[1]['Type']
+        valor_nulo = variavel[1]['Null if']
+        df[nome] = df[0].str.slice(start=pos, stop=pos+tam).str.strip()
+        if not np.isnan(valor_nulo):
+            df[nome] = df[nome].str.replace(valor_nulo, '')
+        if tipo == 'Integer':
+            df[nome] = pd.to_numeric(df[nome], downcast='unsigned', errors='coerce')
+    df.drop(columns=0, inplace=True)
+    dfs.append(df)
+
+df = pd.concat(dfs)
+del dfs
+
+# %%
+df.to_csv(r'C:\arquivos_spark\seer_bruto.csv', index=False)
